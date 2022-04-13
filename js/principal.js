7303298b2877ecb798839eb07a2d8ae597946958
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-04-13 11:13:48 -0300

    operadores logicos, validacoes, e notificacoes p usuario

--- js/principal.js
+++ js/principal.js
@@ -2,3 +2,32 @@ console.log("fui carregado de um arquivo.js externo");
 
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Liliane Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValido = true;
+
+if(peso <=0 || peso >= 1000){
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!"
+}
+
+if(altura <=0 || altura>=3.00){
+    alturaEhValido = false;
+    tdImc.textContent ="Altura inválida";
+}
+
+if(alturaEhValido && pesoEhValido){
+    var imc = peso / (altura * altura);
+    
+    tdImc.textContent = imc;
+}
