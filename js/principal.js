bcdfee30560459c78ed1272f5dd182a38900247f
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-04-18 11:42:56 -0300

    eventos, formularios e add novos elementos na tabela

--- js/principal.js
+++ js/principal.js
@@ -5,7 +5,7 @@ titulo.textContent = "Liliane Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
-console.log(pacientes)
+// console.log(pacientes)
 
 for(var i =0; i<pacientes.length; i++){
 
@@ -47,7 +47,46 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
     console.log('oi cliquei no botao');
     event.preventDefault();
-});
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value; 
+    var peso = form.peso.value ;
+    var altura = form.altura.value ; 
+    var gordura = form.gordura.value;
+    
+     console.log(nome, peso, altura, gordura);
+
+     var pacienteTr = document.createElement("tr");
+
+     var nomeTd = document.createElement("td");
+     var pesoTd = document.createElement("td");
+     var alturaTd = document.createElement("td");
+     var gorduraTd = document.createElement("td");
+     var imcTd = document.createElement("td");
+ 
+    nomeTd.textContent = nome;
+     pesoTd.textContent = peso;
+     alturaTd.textContent = altura;
+     gorduraTd.textContent = gordura;
+
+
+     pacienteTr.appendChild(nomeTd);
+     pacienteTr.appendChild(pesoTd);
+     pacienteTr.appendChild(alturaTd);
+     pacienteTr.appendChild(gorduraTd);
+
+     console.log(pacienteTr);
+
+     var tabela = document.querySelector("#tabela-pacientes");
+
+     tabela.appendChild(pacienteTr);;
+
+
+
+
+    });
+
 
 titulo.addEventListener("click", function(){
     console.log('clickado');
