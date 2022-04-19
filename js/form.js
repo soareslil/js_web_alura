7b672ca0c9151b7f1593fbfbbf34e825d926e755
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-04-18 21:00:57 -0300

    reset do form

--- js/form.js
+++ js/form.js
@@ -17,34 +17,38 @@ botaoAdicionar.addEventListener("click", function (event) {
 
   tabela.appendChild(pacienteTr);
 
-  function obtemPacienteDoFormulario(form) {
-    var paciente = {
-      nome: form.nome.value,
-      peso: form.peso.value,
-      altura: form.altura.value,
-      gordura: form.gordura.value,
-      imc: calculaImc(form.peso.value, form.altura.value),
-    };
-    return paciente;
-  }
-
-  function montaTr(paciente) {
-    var pacienteTr = document.createElement("tr");
-    pacienteTr.classList.add("paciente");
-
-    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
-    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
-    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
-    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
-    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
-
-    return pacienteTr;
-  }
-
-  function montaTd(dado, classe) {
-    var td = document.createElement("td");
-    td.textContent = dado;
-    td.classList.add(classe);
-    return td;
-  }
+  form.reset();
 });
+
+
+
+function obtemPacienteDoFormulario(form) {
+  var paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calculaImc(form.peso.value, form.altura.value),
+  };
+  return paciente;
+}
+
+function montaTr(paciente) {
+  var pacienteTr = document.createElement("tr");
+  pacienteTr.classList.add("paciente");
+
+  pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+  pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+  pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+  pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+  pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+  return pacienteTr;
+}
+
+function montaTd(dado, classe) {
+  var td = document.createElement("td");
+  td.textContent = dado;
+  td.classList.add(classe);
+  return td;
+}
