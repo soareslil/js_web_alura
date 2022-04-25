9d593a840b640f359ca4b2780079f4f49d45262f
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-04-25 15:08:08 -0300

    add regular expression

--- js/filtrar-paciente.js
+++ js/filtrar-paciente.js
@@ -9,8 +9,9 @@ campoFiltro.addEventListener("input", function(){
             let paciente = pacientes[i];
             let tdNome = paciente.querySelector(".info-nome");
             let nome = tdNome.textContent;
-           
-            if(nome != this.value){
+            let expressaoRegular = new RegExp(this.value, "i");
+            
+            if(!expressaoRegular.test(nome)){
                 paciente.classList.add("invisivel");
             }else{
                 paciente.classList.remove("invisivel");
