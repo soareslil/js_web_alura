83795c006276703393b182b39453c939bcc9ceb3
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-04-25 11:35:14 -0300

    animando a remoção do paciente

--- js/remover-pacientes.js
+++ js/remover-pacientes.js
@@ -5,5 +5,9 @@ let tabela = document.querySelector("table");
 tabela.addEventListener("dblclick", function(event){
     let alvoEvento = event.target;
     let paiDoAlvo = alvoEvento.parentNode;
+    paiDoAlvo.classList.add("fadeOut");
+
+    setTimeout(function(){
     paiDoAlvo.remove();
-})
+    }, 800);
+});
